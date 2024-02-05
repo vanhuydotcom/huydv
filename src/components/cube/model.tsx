16145123cda51faa83e85938c0cb2057600c51c5
Huy <dvhuy.dev@gmail.com>
2024-02-05 23:54:48 +0700

    backup

--- src/components/cube/model.tsx
+++ src/components/cube/model.tsx
@@ -2,34 +2,26 @@
 
 import { memo, useRef } from "react";
 import { useFrame, useLoader } from "@react-three/fiber";
-import {
-  BufferGeometry,
-  Group,
-  MathUtils,
-  Mesh,
-  Mesh as MeshInstance,
-  NormalBufferAttributes,
-  Object3DEventMap,
-  TextureLoader,
-} from "three";
-import { Cone } from "@react-three/drei";
+import { Group, MathUtils, Object3DEventMap, TextureLoader } from "three";
+import { Tetrahedron } from "@react-three/drei";
 
 function Model() {
   const groupRef = useRef<Group<Object3DEventMap>>(null);
   useFrame((state, delta) => {
     if (!groupRef.current?.rotation) return;
-    // groupRef.current.rotation!.x += delta * 0.01;
+    // groupRef.current.rotation!.x += delta * 0.2;
     groupRef.current.rotation!.y += delta * 0.2;
-    // groupRef.current.rotation!.z += delta * 0.01;
+    // groupRef.current.rotation!.z += delta * 0.2;
   });
 
-  const texture = useLoader(TextureLoader, "/sapphire-blue-texture.jpg");
+  const texture = useLoader(TextureLoader, "/blue-texture.webp");
 
   return (
     <group ref={groupRef} dispose={null}>
-      <Cone args={[1.5, 2, 4]}>
-        <meshStandardMaterial map={texture} />
-      </Cone>
+      <mesh>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshStandardMaterial color={"#1C85C0"} />
+      </mesh>
     </group>
   );
 }
